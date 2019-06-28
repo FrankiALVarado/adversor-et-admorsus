d87e6bbe56bce554b1e05eb699cd01b96a3aedc9
frank <idv16c.falvarado@uartesdigitales.edu.mx>
2019-06-28 00:46:11 -0500

    Add Initialize Server function

--- Chat p2p/Chat p2p/CServer.cpp
+++ Chat p2p/Chat p2p/CServer.cpp
@@ -4,9 +4,23 @@
 
 CServer::CServer()
 {
+	m_port = 54000;
+	m_ipAdress = "localhost";
 }
-
 
 CServer::~CServer()
 {
+}
+
+void CServer::initializeServer()
+{
+	// Initialize WinSock
+	WSAData data;
+	WORD ver = MAKEWORD(2, 2);
+	int wsResult = WSAStartup(ver, &data);
+	if (wsResult != 0)
+	{
+		cerr << "Can't start Winsock, Err #" << wsResult << endl;
+		return;
+	}
 }
--- Chat p2p/Chat p2p/CServer.h
+++ Chat p2p/Chat p2p/CServer.h
@@ -1,8 +1,22 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+using std::string;
+using std::cerr;
+using std::endl;
 class CServer
 {
+private:
+	int m_port;
+	string m_ipAdress;
+	
 public:
 	CServer();
 	~CServer();
+
+	void initializeServer();
 };
 
